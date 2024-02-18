8df634370999c09bdaaa9bc14d39dc9fddb4cef2
NickArcurve <nick.savino@arcurve.com>
2024-02-18 08:24:32 -0700

    Added Logout Button

--- src/models/user/user.model.js
+++ src/models/user/user.model.js
@@ -4,8 +4,7 @@ const userModel = {
     createdAt: new Date(),
     email: '', 
     stocks: [
-        
-        { ticker: '', shares: 0, purchasePrice: 0.0 }
+        { ticker: '', shares: 0, purchasePrice: 0.0, purchaseDate: new Date() }
     ],
     dollarsEarned: 0
 };
