066993557af22b44979189943ecd2d6503934086
Sebastian Contreras <seb.contreras24@gmail.com>
2024-02-17 21:43:22 -0700

    fixed merge conflicts

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -5,7 +5,6 @@ import styles from "../styles/styles";
 import { SearchBar } from "@rneui/themed"
 import { useState, useEffect } from "react";
 import Explore from "../components/Explore/Explore"
-import { useRouter } from "expo-router";
 import { Button } from "@rneui/base";
 import PreviewCard from "../components/cards/PreviewCard";
 
@@ -31,8 +30,6 @@ const Home = () => {
                 <Explore>
 
                 </Explore>
-                <Text style={styles.title}>Hello World</Text>
-                <Text style={styles.subtitle}>This is the first page of your app.</Text>
                 <PreviewCard preview={mockPathTile}></PreviewCard>
             </View>
         </View>
