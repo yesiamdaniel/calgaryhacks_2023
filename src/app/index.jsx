ce8d8aecbced084c28864c5dd00fcb198117d7d6
desmond.obrien <des@dess-air.lan>
2024-02-18 08:27:04 -0700

    fixing fuck up

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,99 +1,36 @@
-// import Home from "../pages/Home/Home";
-import LessonComplete from "../components/lessons/LessonComplete"
-import { ImageBackground,  Touchable, View, StyleSheet, Dimensions, ScrollView, SafeAreaView, FlatList } from "react-native";
-import { SearchBar, Text } from "@rneui/themed"
-import { useState, useEffect } from "react";
-import { Button } from "@rneui/base";
-import  { initializeApp} from 'firebase/app';
-import { useRouter } from "expo-router";
-import  { getFirestore, collection, query, where, getDocs, setDoc, doc, updateDoc, FieldValue, increment } from 'firebase/firestore';
-import { firebaseConfig, app, db } from "../constants/firebase";
-import { desKey } from "../constants/userKeys";
-import Lesson from "../components/lessons/Lesson";
-import { lesson2 } from "../constants/Lessons";
+import { Redirect } from "expo-router";
+import { useFonts } from 'expo-font';
+import { useCallback } from "react";
 
+import { en, registerTranslation } from "react-native-paper-dates";
+import { useState } from "react";
 
-const HomeTab = () => {
+const Entry = () => {
+    const loggedIn = false;
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    })
 
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
 
+    if (!fontsLoaded) return null;
 
+    return (
+        <>
+            {loggedIn === true ? (
+                <Redirect href='(tabs)/Home' />
+            ) : (
+                <Redirect href='landing/Login' />
+            )}  
+        </>
+    );
 
-  const [path1Data, setPath1Data] = useState(undefined);
-  const [module1Data, setModule1Data] = useState(undefined);
-  const [lessonsData, setLessonsData] = useState([]);
-  const [hasLoaded, setHasLoaded] = useState(false);
- 
-
-  const path1_data = () => {
-          const q = query(collection(db, "Path1/"), where("id", "==", 0));
-          const docs = getDocs(q).then((docs) => {  
-          docs.forEach((x) => {
-              setPath1Data(x.data());
-              
-          })             
-  });
 }
 
-  const module1_data = () => {
-      const q = query(collection(db, "Path1/Path1/Module1.1"), where("id", "==", 0));
-
-      const docs = getDocs(q).then((docs) => {  
-
-      docs.forEach((x) => {
-              setModule1Data(x.data());
-          })             
-      });
-
-  }
-
-  const lessons_data = () => {
-      const q = query(collection(db, "Path1/Path1/Module1.1"), where("id", ">", 0));
-
-      var arr = [];
-      const docs = getDocs(q).then((docs) => {  
-
-      var arr = [];
-
-      docs.forEach((x) => {
-              arr.push(x.data());
-          })             
-      });
-      setLessonsData(arr);
-  }
-
-  
-  
-
-
-  useEffect(() => {
-    /*
-      if (path1Data == undefined) {
-          path1_data();
-      }
-      if (module1Data == undefined) {
-          module1_data();
-      }
-      if (lessonsData.length === 0) {
-          lessons_data();
-      }
-      if (lessons_data.length > 0) {
-
-      }
-      */
-  },[]); 
-
-
-
-
-
-  return (
-
-       <Lesson info={lesson2}></Lesson>
-
-
-   
-
-  )
-}
-
-export default HomeTab
+export default Entry;
