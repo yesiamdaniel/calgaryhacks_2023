6ef28f8d82f885ec6a3b26e2cfb1924980e182fd
Daniel Appiah <danielappiah118@gmail.com>
2024-02-18 04:44:04 -0700

    fonts added

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,25 @@
 import { Redirect } from "expo-router";
+import { useFonts } from 'expo-font';
+import { useCallback } from "react";
+
 
 const Entry = () => {
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) return null;
+
     return (
-        <Redirect href='(tabs)/Home' />
+        <Redirect onLayoutRootView={onLayoutRootView} href='(tabs)/Home' />
     );
 
 }
--- src/constants/themes/colors.js
+++ src/constants/themes/colors.js
@@ -49,3 +49,5 @@ const COLORS = {
     }
 
 }
+
+export { COLORS };
